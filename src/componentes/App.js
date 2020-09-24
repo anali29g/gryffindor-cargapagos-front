92468d3a9e8cf1a5bf7cb3343d52e782970d860f
anali29g <56572614+anali29g@users.noreply.github.com>
2020-09-23 23:26:22 -0500

    version 1.2

---
+++ src/componentes/App.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './Login';
+import ModuloCarga from './ModuloCarga';
+import {ProtectedRoute} from "./Protected.route";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <React.Fragment>
+                <Route exact path="/" component={Login} />
+                <ProtectedRoute exact path="/modulo-carga" component={ModuloCarga}/>
+            </React.Fragment>
+        </BrowserRouter>
+    );
+}
+
+export default App;
